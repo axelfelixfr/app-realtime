dc4eee982a4b9bfae4d74b445bc9309159891af3
Axel <axelfelix123@hotmail.com>
2021-08-12 22:47:28 -0500

    Services de user y reestructuracion de endpoint user

--- imports/api/Users/UsersController.js
+++ imports/api/Users/UsersController.js
@@ -1,11 +1,17 @@
 import { ValidatedMethod } from "meteor/mdg:validated-method";
 import { check, Match } from "meteor/check";
+import { Accounts } from "meteor/accounts-base";
+import { ResponseMessage } from "../../startup/server/utilities/ResponseMessage";
+import UsersServices from "./UsersServices";
 
 new ValidatedMethod({
   name: "saveUser",
+  // Primero validamos el user que llego
   validate(user) {
     try {
+      // Con check validamos los datos del user
       check(user, {
+        // El _id puese ser un string (se trata de actualizar registro, UPDATE) o null (se trata de un nuevo registro, INSERT)
         _id: Match.OneOf(String, null),
         username: String,
         emails: [{ address: String, verified: Boolean }],
@@ -16,24 +22,75 @@ new ValidatedMethod({
         }
       });
     } catch (exception) {
-      console.log("saveUser: ", exception);
+      console.error("saveUser: ", exception);
       throw new Meteor.Error("403", "Los datos son incorrectos");
     }
+    // Validamos el correo y el username
+    UsersServices.validateEmail(user.emails[0].address, user._id);
+    UsersServices.validateUsername(user.username, user._id);
   },
-  run({ username, emails, profile }) {
+  // Después realizamos el proceso de base de datos
+  // Desestructuramos user, ya que user = { _id, username, emails, profile }
+  run({ _id, username, emails, profile }) {
     console.log("user: ", { username, emails, profile });
-    let responseMessage = "";
-    try {
-      Accounts.createUser({
-        username,
-        email: emails[0].address,
-        profile
-      });
-      responseMessage = "Se ha creado el usuario correctamente";
-    } catch (exception) {
-      console.log("saveUser: ", exception);
-      throw new Meteor.Error("500", "Ocurrió un error al crear el usuario");
+    const responseMessage = new ResponseMessage(); // Inicializamos la clase ReponseMessage
+
+    // Si el _id es diferente a null, significa que se trata de una consulta UPDATE
+    if (_id !== null) {
+      // Realizamos un try/catch para cachar los errores
+      try {
+        UsersServices.updateUser(_id, username, emails, profile);
+
+        // Creamos el mensaje al finalizar el proceso
+        responseMessage.create("Se ha actualizado el usuario correctamente");
+      } catch (exception) {
+        console.error("saveUser: ", exception);
+        throw new Meteor.Error(
+          "500",
+          "Ocurrió un error al actualizar el usuario"
+        );
+      }
+
+      // Al no entrar a la primera condición, entonces se trata de un nuevo registro, INSERT
+    } else {
+      try {
+        UsersServices.createUser(username, emails, profile);
+
+        // Creamos el mensaje al finalizar el proceso
+        responseMessage.create("Se ha creado el usuario correctamente");
+      } catch (exception) {
+        console.error("saveUser: ", exception);
+        throw new Meteor.Error("500", "Ocurrió un error al crear el usuario");
+      }
     }
+
+    // Retornamos el mensaje por último
+    return responseMessage;
+  }
+});
+
+new ValidatedMethod({
+  name: "deleteUser",
+  validate({ idUser }) {
+    try {
+      check(idUser, String);
+    } catch (exception) {
+      console.error("deleteUser: ", exception);
+      throw new Meteor.Error("403", "La información introducida es incorrecta");
+    }
+  },
+  run({ idUser }) {
+    const responseMessage = new ResponseMessage(); // Inicializamos la clase ReponseMessage
+    try {
+      Meteor.users.remove(idUser);
+      // Creamos el mensaje al finalizar el proceso
+      responseMessage.create("Se ha eliminado el usuario correctamente");
+    } catch (exception) {
+      console.error("deleteUser: ", exception);
+      throw new Meteor.Error("500", "Ocurrió un error al eliminar el usuario");
+    }
+
+    // Retornamos el mensaje por último
     return responseMessage;
   }
 });
---
+++ imports/api/Users/UsersServices.js
@@ -0,0 +1,93 @@
+import { Accounts } from "meteor/accounts-base";
+
+export default {
+  validateEmail(newEmail, idUser) {
+    // Buscamos el correo que llego por los parámetros con findUserByEmail()
+    const existsEmail = Accounts.findUserByEmail(newEmail);
+
+    // Si el idUser existe, significa que se trata de una actualización, UPDATE
+    if (idUser) {
+      // Desestructuramos el user que se obtuvo con idUser
+      const { emails: emailsDB } = Meteor.users.findOne(idUser);
+
+      // Comparamos el correo de la base de datos (emailsDB) con el newEmail
+      // Además de que comprobamos de que exista el correo
+      if (emailsDB[0].address !== newEmail && existsEmail) {
+        // Lanza el error
+        throw new Meteor.Error("403", "El nuevo email ya existe");
+      }
+
+      // Si no entro a la primera condición, se trata se un nuevo registro, INSERT
+    } else if (existsEmail) {
+      // Lanza el error
+      throw new Meteor.Error("403", "El email ya existe");
+    }
+  },
+  validateUsername(newUsername, idUser) {
+    // Buscamos el username que llego por los parámetros con findUserByUsername()
+    const existsUsername = Accounts.findUserByUsername(newUsername);
+
+    // Si el idUser existe, significa que se trata de una actualización, UPDATE
+    if (idUser) {
+      // Desestructuramos el user que se obtuvo con el metodo findOne
+      const { username: usernameDB } = Meteor.users.findOne(idUser);
+
+      // Comparamos el username de la base de datos (usernameDB) con el newUsername
+      // Además de que comprobamos de que exista el username
+      if (usernameDB !== newUsername && existsUsername) {
+        // Lanza el error
+        throw new Meteor.Error("403", "El nuevo nombre de usuario ya existe");
+      }
+      // Si no entro a la primera condición, se trata se un nuevo registro, INSERT
+    } else if (existsUsername) {
+      // Lanza el error
+      throw new Meteor.Error("403", "El nombre de usuario ya existe");
+    }
+  },
+  createUser(username, emails, profile) {
+    // username, email y name (de profile) deben ser únicos, ya que de esta forma lo pide Accounts.createUser()
+    Accounts.createUser({
+      username,
+      email: emails[0].address,
+      profile
+    });
+  },
+  updateUser(_id, username, emails, profile) {
+    // Desestructuramos el objeto que nos trae users.findOne() que se obtiene de la base de datos de Mongo
+    const {
+      _id: idDB,
+      username: usernameDB,
+      emails: emailsDB
+    } = Meteor.users.findOne(_id);
+    // Con findOne() nos trae un objeto con el _id que le corresponde en la base de datos
+
+    // Si el email (que se envio) es diferente al email de la base de datos, entra a la condición
+    if (emailsDB[0].address !== emails[0].address) {
+      // Removemos el email con removeEmail()
+      Accounts.removeEmail(idDB, emailsDB[0].address);
+      // Le pasamos el id de la base de datos y el correo que queremos remover
+
+      // Agregamos el nuevo email
+      Accounts.addEmail(idDB, emails[0].address);
+      // Para ello pasamos el id de la base de datos y el nuevo correo que se envio
+    }
+
+    // Si el username (que se envio) es diferente al username de la base de datos, entra a la condición
+    if (usernameDB !== username) {
+      // Accedemos con el id de la base de datos y le pasamos el nuevo username
+      Accounts.setUsername(idDB, username);
+    }
+
+    // Para cambiar los demás datos del user, usamos Meteor.users.update()
+    Meteor.users.update(_id, {
+      // Con "$set" insertamos los datos de profile a la base de datos
+      $set: {
+        profile: {
+          profile: profile.profile,
+          name: profile.name,
+          path: profile.path
+        }
+      }
+    });
+  }
+};
